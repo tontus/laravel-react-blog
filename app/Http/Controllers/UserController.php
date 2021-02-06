47b626aad81a2b29b08e35ea7fe60d2f562cdf43
tontus <tonmoyshaleh@gmail.com>
2021-02-07 00:30:31 +0600

    user crud functions created

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return User[]|\Illuminate\Database\Eloquent\Collection
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return $users;
     }
 
     /**
@@ -30,47 +35,98 @@ class UserController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $formData = $request->all();
+        $validator = Validator::make($formData, [
+            'name' => ['required','max:100'],
+            'username' => ['required','unique:users'],
+            'email' => ['required','unique:users'],
+//            'password'=>['required']
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'success'=> false,
+                'message'=>$validator->errors(),
+            ]);
+        }
+        $user = New User();
+        $user->name =$request->name;
+        $user->username =$request->username;
+        $user->email =$request->email;
+        $user->password ='$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+//        $user->email_verified_at = now();
+//        $user->remember_token = Str::random(10);
+        $user->save();
+        return response()->json([
+            'success'=> true,
+            'message'=>'user saved',
+            'data'=> $user,
+        ]);
+
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        return response()->json([
+            'success'=> true,
+            'message'=>'User details',
+            'data'=>$user
+        ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,$id)
     {
-        //
-    }
+        $formData = $request->all();
+        $user = User::find($id);
+        if(is_null($user)){
+            return response()->json([
+                'success'=> false,
+                'message'=>'user not found',
+                'data'=>null
+            ]);
+        }
+        $validator = Validator::make($formData, [
+            'name' => ['required','max:100'],
+            'username' => ['required'],
+            'email' => ['required'],
+//            'password'=>['required']
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'success'=> false,
+                'message'=>$validator->errors(),
+            ]);
+        }
+        $user->name =$request->name;
+        $user->username =$request->username;
+        $user->email =$request->email;
+        $user->password ='$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+//        $user->email_verified_at = now();
+//        $user->remember_token = Str::random(10);
+        $user->save();
+        return response()->json([
+            'success'=> true,
+            'message'=>'user saved',
+            'data'=> $user,
+        ]);
 
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -80,5 +136,14 @@ class UserController extends Controller
     public function destroy($id)
     {
         //
+    }
+    public function userPosts($id)
+    {
+        $posts = Post::where('user_id',$id) ->get();
+        return response()->json([
+            'success'=> true,
+            'message'=>'post retrieved',
+            'data'=> $posts,
+        ]);
     }
 }
